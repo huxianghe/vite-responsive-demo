9c5e98b598861b74cc192ee86c381bb79a74ecb1
aedrian <34914195+huxianghe@users.noreply.github.com>
2023-04-19 10:09:18 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,26 +3,26 @@ import ReactDOM from 'react-dom/client';
 import { createHashRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 import './index.css';
-// import BaseForm from './components/Form';
-// import BaseCard from './components/Card';
+import BaseForm from './components/Form';
+import BaseCard from './components/Card';
 
 const router = createHashRouter([
   {
     path: '/',
     element: <App />,
     children: [
-      // {
-      //   index: true,
-      //   element: <BaseForm />,
-      // },
-      // {
-      //   path: 'form',
-      //   element: <BaseForm />,
-      // },
-      // {
-      //   path: 'card',
-      //   element: <BaseCard />,
-      // },
+      {
+        index: true,
+        element: <BaseForm />,
+      },
+      {
+        path: 'form',
+        element: <BaseForm />,
+      },
+      {
+        path: 'card',
+        element: <BaseCard />,
+      },
     ],
   },
 ]);
